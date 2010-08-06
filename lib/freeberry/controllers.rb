a2bf4c65c63d679306ffbe10465b6faddc5f1d0b
superp <superp1987@gmail.com>
2010-08-06 20:55:41 +0800

    Remove no more need controllers.rb

--- lib/freeberry/controllers.rb
+++
@@ -1,8 +0,0 @@
-# encoding: utf-8
-module Freeberry
-  module Controllers
-    autoload :AuthorizedSystem, 'freeberry/controllers/authorized_system'
-    autoload :HeadOptions,      'freeberry/controllers/head_options'
-    autoload :HelperTools,      'freeberry/controllers/helper_tools'
-  end
-end
