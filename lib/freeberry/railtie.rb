1b1d3191682d6fdd4200e174e9a6c833603c8fe0
superp <superp@superp-laptop.(none)>
2010-08-06 15:51:06 +0300

    Replace railtie to engine && add module Controllers

--- lib/freeberry/railtie.rb
+++ lib/freeberry/railtie.rb
@@ -1,32 +1,30 @@
 # encoding: utf-8
+require 'rails'
 require 'freeberry'
 
 module Freeberry
-  if defined? Rails::Railtie
-    require 'rails'
-    class Railtie < Rails::Railtie      
-      config.before_initialize do
-        ActiveSupport::XmlMini.backend = 'Nokogiri'
-        I18n::Backend::Simple.send(:include, I18n::Backend::Pluralization)
-          
-        config.i18n.load_path += Dir[File.join(File.dirname(__FILE__), "../../config", 'locales', 'defaults', '*.{rb,yml}').to_s]
-        config.i18n.load_path += Dir[File.join(File.dirname(__FILE__), "../../config", 'locales', 'manage', '*.{rb,yml}').to_s]
+  class Engine < ::Rails::Engine   
+    config.before_initialize do
+      ActiveSupport::XmlMini.backend = 'Nokogiri'
+      I18n::Backend::Simple.send(:include, I18n::Backend::Pluralization)
         
-        ActiveSupport.on_load :active_record do
-          ActiveRecord::Base.send :include, Freeberry::MysqlUtils
-        end
-      end
+      config.i18n.load_path += Dir[File.join(File.dirname(__FILE__), "../../config", 'locales', 'defaults', '*.{rb,yml}').to_s]
+      config.i18n.load_path += Dir[File.join(File.dirname(__FILE__), "../../config", 'locales', 'manage', '*.{rb,yml}').to_s]
       
-      config.after_initialize do
-        ActionController::Base.send :include, Freeberry::Controllers::AuthorizedSystem
-        ActionController::Base.send :include, Freeberry::Controllers::HeadOptions
-      
-        ActionView::Base.send :include, Freeberry::Controllers::HelperTools
+      ActiveSupport.on_load :active_record do
+        ActiveRecord::Base.send :include, Freeberry::MysqlUtils
       end
-      
-      rake_tasks do
-        load "tasks/freeberry.rake"
-      end
+    end
+    
+    config.after_initialize do
+      ActionController::Base.send :include, Freeberry::Controllers::AuthorizedSystem
+      ActionController::Base.send :include, Freeberry::Controllers::HeadOptions
+    
+      ActionView::Base.send :include, Freeberry::Controllers::HelperTools
+    end
+    
+    rake_tasks do
+      load "tasks/freeberry.rake"
     end
   end
 end
