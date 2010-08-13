73117b3a87bd980c9482c6f258f16fdd73edfb95
Igor Galeta <galeta.igor@gmail.com>
2010-08-13 16:59:30 +0300

    Add ukraine locale for devise

--- lib/generators/freeberry/models/templates/models/user.rb
+++ lib/generators/freeberry/models/templates/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :confirmable, :lockable, :timeoutable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  attr_accessible :email, :password, :password_confirmation
+  attr_accessible :name, :email, :password, :password_confirmation
   
 #  acts_as_attach_file :avatar
   
