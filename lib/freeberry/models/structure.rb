fa8e39cb2ef2f3bb08a739129f9594df69edb988
Igor Galeta <galeta.igor@gmail.com>
2010-09-06 11:49:46 +0300

    Move Structure acts_as_nested_set into generated model

--- lib/freeberry/models/structure.rb
+++ lib/freeberry/models/structure.rb
@@ -11,15 +11,11 @@ module Freeberry
         def self.extended(base)
           base.send(:include, HeaderTools)
           base.class_eval do
-            acts_as_nested_set
-            
             enumerated_attribute :structure_type, :id_attribute => :kind
             enumerated_attribute :position_type, :id_attribute => :position
             
             validates_presence_of :title
             validates_numericality_of :position, :only_integer => true
-            
-            has_slug :prepend_id => false
             
             has_one :page, :dependent => :destroy
             has_many :posts, :dependent => :destroy
--- lib/generators/freeberry/models/templates/models/structure.rb
+++ lib/generators/freeberry/models/templates/models/structure.rb
@@ -1,3 +1,6 @@
 class Structure < ActiveRecord::Base
   include Freeberry::Models::Structure
+  
+  acts_as_nested_set
+  has_slug :prepend_id => false
 end
