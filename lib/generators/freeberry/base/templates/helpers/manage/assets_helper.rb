457f3fba9baa9c387fbe5648e1120c743e09d922
Igor Galeta <galeta.igor@gmail.com>
2010-11-24 10:52:29 +0200

    Update jquery to 1.4.4 and rails.js

--- lib/generators/freeberry/base/templates/helpers/manage/assets_helper.rb
+++ lib/generators/freeberry/base/templates/helpers/manage/assets_helper.rb
@@ -1,12 +1,10 @@
 module Manage::AssetsHelper
   
-  def manage_assets_path_with_session_information(klass='Asset', options = {})
-    options.symbolize_keys!
+  def manage_assets_path_with_session_information(klass = 'Asset', options = {})
+    options = { :format => :xml, :protocol => "http://" }.merge(options)
     session_key = Rails.application.config.send(:session_options)[:key]
     
     options[session_key] = Rack::Utils.escape(cookies[session_key])
-    options[:format] = :xml
-    options[:protocol] = "http://"
     options[:klass] = klass
     
     if protect_against_forgery?
